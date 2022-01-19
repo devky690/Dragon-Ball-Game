29e49bef147951d7f69ca0557a3453da97d337b5
Kyle Abrahams <kyleabrahams@Kyles-MacBook-Air.local>
2022-01-19 14:52:33 -0500

    nimbus goku image added

---
+++ my-app/src/Components/Game.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const [gameUp, setGameUp] = useState(false);
+const []
+
+const Game = () => {
+  return <div></div>;
+};
+
+function startGame() {
+  while (gameUp) {
+      
+  }
+}
+
+export default Game;
